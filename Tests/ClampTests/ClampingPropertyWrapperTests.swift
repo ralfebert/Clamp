56ff800d3ec044d2700ec4abe6f02ef439eee2aa
Ralf Ebert <info@ralfebert.de>
2021-09-19 14:07:26 +0200

    Clamping property wrapper implemented

--- Tests/ClampTests/ClampingPropertyWrapperTests.swift
+++ Tests/ClampTests/ClampingPropertyWrapperTests.swift
@@ -1,7 +1,11 @@
 @testable import Clamp
 import XCTest
 
-class ClampPropertyWrapperTests: XCTestCase {
+struct ExampleModel {
+    @Clamping(0 ... 10) var someValue = 5.0
+}
+
+class ClampingPropertyWrapperTests: XCTestCase {
     func testClampModelValue() {
         var model = ExampleModel()
 
@@ -19,5 +23,12 @@ class ClampPropertyWrapperTests: XCTestCase {
 
         model.someValue = 11.0
         XCTAssertEqual(10.0, model.someValue)
+    }
+
+    func testClampVariable() {
+        @Clamping(0 ... 10) var value = -1
+        XCTAssertEqual(0, value)
+        value = 11
+        XCTAssertEqual(10, value)
     }
 }
--- Tests/ClampTests/ExampleModel.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-struct ExampleModel {
-    var someValue = 5.0
-}
